d662368d2389ad5187fef16f95435b896ff45498
Alex Foglia <alex.f1793@gmail.com>
2020-02-22 01:16:10 +0100

    chauvenet3

--- chauvenet/chauvenet.py
+++ chauvenet/chauvenet.py
@@ -3,6 +3,7 @@ from math import erf
 mean = lambda x : sum(x)/len(x)
 sd = lambda x : (sum([(x[i] - mean(x))**2 for i in range(0,len(x))])/(len(x) - 1))**0.5
 sqrt2 = 2**0.5
+cnorm = lambda x : (1 + erf((x)/(sqrt2)))/2
 
 x = [10.1, 10.0, 10.2, 10.3, 10.2, 10.1, 10.0, 10.2, 10.1, 10.2, 10.1, 10.3, 11.0, 11.5, 11.6]
 
@@ -15,7 +16,7 @@ while flag:
     s = sd(x)
     m = mean(x)
     n = len(x)
-    cnorm = lambda x : (1 + erf((x)/(sqrt2)))/2
+
     sds = []
 
     for x_i in x:
